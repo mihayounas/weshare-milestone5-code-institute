e37a8e4ab3da1d9c1c2d18ea2195290487d1f337
mihayounas <76411576+mihayounas@users.noreply.github.com>
2022-11-23 11:24:30 +0000

    Style the navbar to be user friendly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import {Navbar, Container, Nav, NavDropdown,Form,FormControl,Button} from "react-bootstrap";
+import {Navbar, Container, Nav, NavDropdown,FormControl,Button} from "react-bootstrap";
 import styles from "../styles/NavBar.module.css"
 
+
+
 const NavBar = () => {
-  return  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
-    <Container>
-  <Navbar.Brand href="#home">WeShare</Navbar.Brand>
+  return  <Navbar className= {styles.NavBar}  collapseOnSelect expand="lg" bg="primary" variant="dark" fixed="top">
+    <Container className={styles.Tabs}>
+   <Navbar.Brand href="#home"><h1>WeShare</h1></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
+  <Navbar.Collapse id="responsive-navbar-nav" >
     <Nav className="mr-auto">
-    <Nav.Link href="#"><i className="fas fa-home"></i>Home</Nav.Link>
+    <Nav.Link href="#"><i className="fas fa-home"></i> Home</Nav.Link>
+    <Nav.Link href="#"><i class="fa fa-newspaper"></i> Read News</Nav.Link>
    
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
       </Nav> 
     <Nav>
     <Nav.Link href="#"><i className="fas fa-sign-in-alt"></i>SignIn</Nav.Link>
     <Nav.Link href="#"><i className="fas fa-user-plus"></i>SignUp</Nav.Link>
     </Nav>
     <Nav>
-    <FormControl type="text" placeholder="Search" />
+    <FormControl type="text" placeholder="Search Friends" />
     <Button type="submit">Go</Button>
   </Nav>
   </Navbar.Collapse>
