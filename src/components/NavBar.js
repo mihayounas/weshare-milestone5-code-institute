01741d5ec3a4972e1a109c94be65bcd18ff0e389
mihayounas <76411576+mihayounas@users.noreply.github.com>
2022-11-23 11:50:34 +0000

    create routing to wire up link in the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import {Navbar, Container, Nav, NavDropdown,FormControl,Button} from "react-bootstrap";
+import {Navbar, Container, Nav, FormControl,Button} from "react-bootstrap";
 import styles from "../styles/NavBar.module.css"
+import { NavLink } from "react-router-dom"
 
 
 
 const NavBar = () => {
   return  <Navbar className= {styles.NavBar}  collapseOnSelect expand="lg" bg="primary" variant="dark" fixed="top">
-    <Container className={styles.Tabs}>
+    <Container>
+    <NavLink to="/">
    <Navbar.Brand href="#home"><h1>WeShare</h1></Navbar.Brand>
+   </NavLink>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav" >
     <Nav className="mr-auto">
+    <NavLink to="home">
     <Nav.Link href="#"><i className="fas fa-home"></i> Home</Nav.Link>
-    <Nav.Link href="#"><i class="fa fa-newspaper"></i> Read News</Nav.Link>
-   
+    </NavLink>
+    <NavLink to="/">
+    <Nav.Link href="#"><i className="fa fa-newspaper"></i> Read News</Nav.Link>
+    </NavLink>
+
       </Nav> 
     <Nav>
+    <NavLink to="/signin">
     <Nav.Link href="#"><i className="fas fa-sign-in-alt"></i>SignIn</Nav.Link>
+    </NavLink>
+    <NavLink to="/signup">
     <Nav.Link href="#"><i className="fas fa-user-plus"></i>SignUp</Nav.Link>
+    </NavLink>
     </Nav>
     <Nav>
     <FormControl type="text" placeholder="Search Friends" />
