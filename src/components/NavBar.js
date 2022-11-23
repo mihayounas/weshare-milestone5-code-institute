73568a9ccbf25419bc2c27cc9de95450eeb8b634
mihayounas <76411576+mihayounas@users.noreply.github.com>
2022-11-23 00:16:37 +0000

    Adjusting navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap"
+import {Navbar, Container, Nav, NavDropdown,Form,FormControl,Button} from "react-bootstrap";
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
@@ -8,8 +9,8 @@ const NavBar = () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <Nav.Link href="#"><i className="fas fa-home"></i>Home</Nav.Link>
+   
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -17,13 +18,15 @@ const NavBar = () => {
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
+      </Nav> 
+    <Nav>
+    <Nav.Link href="#"><i className="fas fa-sign-in-alt"></i>SignIn</Nav.Link>
+    <Nav.Link href="#"><i className="fas fa-user-plus"></i>SignUp</Nav.Link>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
+    <FormControl type="text" placeholder="Search" />
+    <Button type="submit">Go</Button>
+  </Nav>
   </Navbar.Collapse>
   </Container>
 </Navbar>
