5e8764f27408a6483b68c9a890370e51058d78f6
mihayounas <mihaelayounas55@gmail.com>
2022-12-02 23:47:18 +0000

    creating the sign in and sign up forms for user functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav, FormControl, Button } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
   return (
     <Navbar
       className={styles.NavBar}
@@ -27,18 +49,12 @@ const NavBar = () => {
               <i className="fa fa-newspaper"></i> Read News
             </NavLink>
           </Nav>
-          <Nav>
-            <NavLink activeClassName={styles.Active} to="/signin">
-              <i className="fas fa-sign-in-alt"></i>SignIn
-            </NavLink>
-            <NavLink activeClassName={styles.Active} to="/signup">
-              <i className="fas fa-user-plus"></i>SignUp
-            </NavLink>
-          </Nav>
+
           <Nav>
             <FormControl type="text" placeholder="Search Friends" />
             <Button type="submit">Go</Button>
           </Nav>
+          {currentUser ? loggedInIcons : loggedOutIcons}
         </Navbar.Collapse>
       </Container>
     </Navbar>
