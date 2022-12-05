118fa9d405eee7284f49c72f5874ccd8f4cc6aef
mihayounas <mihaelayounas55@gmail.com>
2022-12-05 22:46:57 +0000

    Add new style to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navbar, Container, Nav, FormControl, Button } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { CurrentUserContext } from "../App";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
+  const currentUser = useCurrentUser();
+
+  const addPostIcon = {
+
+  }
 
   const loggedInIcons = <>{currentUser?.username}</>;
   const loggedOutIcons = (
@@ -13,52 +17,54 @@ const NavBar = () => {
       <NavLink
         className={styles.NavLink}
         activeClassName={styles.Active}
-        to="/signin"
-      >
+        to="/signin">
         <i className="fas fa-sign-in-alt"></i>Sign in
       </NavLink>
       <NavLink
         to="/signup"
         className={styles.NavLink}
-        activeClassName={styles.Active}
-      >
+        activeClassName={styles.Active}>
         <i className="fas fa-user-plus"></i>Sign up
       </NavLink>
     </>
   );
-  return (
-    <Navbar
-      className={styles.NavBar}
-      collapseOnSelect
-      expand="lg"
-      bg="primary"
-      variant="dark"
-      fixed="top"
-    >
-      <Container >
-        <NavLink to="/">
-          <h1>WeShare</h1>
-        </NavLink>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <NavLink activeClassName={styles.Active} to="/">
-              <i className="fas fa-home"></i> Home
-            </NavLink>
-            <NavLink activeClassName={styles.Active} to="/">
-              <i className="fa fa-newspaper"></i> Read News
-            </NavLink>
-          </Nav>
 
-          <Nav>
-            <FormControl type="text" placeholder="Search Friends" />
-            <Button type="submit">Go</Button>
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <h1>WeShare</h1>
+          </Navbar.Brand>
+        </NavLink>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
+
           </Nav>
-          {currentUser ? loggedInIcons : loggedOutIcons}
+          <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/news">
+            <i class="fa-regular fa-newspaper"></i>News
+          </NavLink>
         </Navbar.Collapse>
       </Container>
+      <Nav>
+        <FormControl type="text" placeholder="Search" />
+        <Button type="submit">Go</Button>
+      </Nav>
     </Navbar>
   );
 };
-
 export default NavBar;
