6654060ed464acfdee7c9b1b128109d925f48a46
mihayounas <mihaelayounas55@gmail.com>
2022-12-09 13:17:56 +0000

    change axios default

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://weshare-api-app.herokuapp.com/";
+axios.defaults.baseURL = "https://weshare-media.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
