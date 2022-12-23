30d326a2c08119c74f24b9d4398e7cd9d19f6d1a
mihayounas <mihaelayounas55@gmail.com>
2022-12-23 00:56:23 +0000

    putting in place news page

--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -1,57 +1,14 @@
 import React from "react";
-import styles from "../../styles/Post.module.css";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
-import Avatar from "../../components/Avatar";
-import { axiosRes } from "../../api/axiosDefaults";
-import { MoreDropdown } from "../../components/MoreDropdown";
 
-const News = (props) => {
-  const {
-    id,
-    owner,
-    profile_id,
-    profile_image,
-  } = props;
 
-  const currentUser = useCurrentUser();
-  const is_owner = currentUser?.username === owner;
-  const history = useHistory();
+const News = ({ }) => {
 
-  const handleEdit = () => {
-    history.push(`/posts/${id}/edit`);
-  };
-
-  const handleDelete = async () => {
-    try {
-      await axiosRes.delete(`/posts/${id}/`);
-      history.goBack();
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
-    <Card className={styles.Post}>
-      <Card.Body>
-        <Media className="align-items-center justify-content-between">
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
-            {owner}
-          </Link>
-          <div className="d-flex align-items-center">
-            <span>{updated_at}</span>
-            {is_owner && postPage &&
-              <MoreDropdown
-                handleEdit={handleEdit}
-                handleDelete={handleDelete}
-              />}
-          </div>
-        </Media>
-      </Card.Body>
-
-    </Card>
+    
+   <div>
+    <h2>hey look its news</h2>
+   </div>
   );
 };
 
--- src/pages/news/NewsPage.js
+++ src/pages/news/NewsPage.js
@@ -1,36 +1,17 @@
 import React, { useEffect, useState } from "react";
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import { Button } from "react-bootstrap";
-
-import appStyles from "../../App.module.css";
-import { useParams } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
-
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
-
-import InfiniteScroll from "react-infinite-scroll-component";
-
-import { fetchMoreData } from "../../utils/utils";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import axios from 'axios';
+import News from "./News";
 
 function NewsPage() {
-    const { id } = useParams();
 
-    const currentUser = useCurrentUser();
-    const profile_image = currentUser?.profile_image;
-
+   
 
     return (
-        <Row className="h-100">
-            <Col className="py-2 p-0 p-lg-2" lg={8}>
-                <p>News</p>
-            </Col>
-            <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-                Popular news
-            </Col>
-            <Button>Add News</Button>
-        </Row>
+        <div>
+            <News />
+        </div>
     );
 }
 
