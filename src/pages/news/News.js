26efa6ef6298af85b80b8093deb205372f0a6ab7
mihayounas <mihaelayounas55@gmail.com>
2022-12-24 01:37:29 +0000

    working on displaying news and style it acordingly

--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Collapse from 'react-bootstrap/Collapse';
+import { Container, Col, Row } from 'react-bootstrap';
 
 
 const News = ({ newsItem }) => {
-
-
-
+  const [open, setOpen] = useState(false);
   return (
-    
-   <div>
-    <h2>{newsItem.title}</h2>
-    <p>{newsItem.description}</p>
-   </div>
+    <Container className="news-item-container">
+      <Row>
+        <Col>
+          <div style={{
+            display: 'grid', gridAutoRows: 'auto', gridGap: '1rem', gridTemplateColumns: 'repeat(auto-fill, 250px)',
+          }}>
+            <Card style={{ width: '18rem', border: '2px solid #e7e7e7', borderRadius: '4px', padding: '.5rem' }}>
+              <Card.Img variant="top" src={newsItem.image_url} />
+              <Card.Body>
+                <Card.Title>{newsItem.title}</Card.Title>
+                <>
+                  <Button
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                  >
+                    Read More
+                  </Button>
+                  <Collapse in={open}>
+                    <div id="example-collapse-text">
+                      {newsItem.description}
+                    </div>
+                  </Collapse>
+                </>
+              </Card.Body>
+            </Card>
+          </div >
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/news/NewsPage.js
+++ src/pages/news/NewsPage.js
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import axios from 'axios';
 import News from "./News";
+import { Col, Row } from 'react-bootstrap';
 
 function NewsPage() {
-    const url = `https://newsdata.io/api/1/news?apikey=${process.env.REACT_APP_NEWS_API_KEY}&country=gb&language=en&category=health`
+    const url = `https://newsdata.io/api/1/news?apikey=${process.env.REACT_APP_NEWS_API_KEY}&country=gb&language=en&category=food`
 
     const [news, setNews] = useState([])
 
     useEffect(() => {
         const getNews = async () => {
-
             const response = await fetch(url)
             const result = await response.json()
-
             setNews(result.results)
-
-
         }
         getNews()
 
@@ -25,10 +19,14 @@ function NewsPage() {
 
     return (
 
-        <div>
-            {console.log(news)}
-            {news.map(newsItem => <News newsItem={newsItem} />)}
-        </div>
+        <Row>
+            {news.map(newsItem => (
+                <Col key={newsItem.id} xs={6} md={4}>
+                    <News newsItem={newsItem} />
+                </Col>
+            ))}
+        </Row>
+
     );
 }
 
